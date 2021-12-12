7bcd26577a3e5908b88c67b47e1c29ab99a6acc0
KorryKatti <60970528+korrykatti@users.noreply.github.com>
2021-12-12 14:06:35 +0530

    *interesting*

--- waifu.py
+++ waifu.py
@@ -1,10 +1,18 @@
 import os
 import time
+import wave
 import pyaudio
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
 
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "x.wav"
 
 def speak(text):
     tts = gTTS(text=text, lang="en-in")
@@ -13,19 +21,30 @@ def speak(text):
     playsound.playsound(filename)
 
 def get_audio():
-    vocbrain = sr.Recognizer()
-    with sr.Microphone as source:
-        audio = vocbrain.listen(source)
-        said = ""
+    file = open("microphone-results.wav", "w")
+    os.remove('microphone-results.wav')
+    
+    r = sr.Recognizer()
+    with sr.Microphone(sample_rate = 48000) as source:
+        print("Now Listening")
+        audio = r.record(source, duration = 5)
+    with open("microphone-results.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+            r.pause_threshold = 1
+    with sr.AudioFile("microphone-results.wav") as source:
+            audio = r.listen(source)
+
+    try:
+        said = r.recognize_google(audio, language='en-in')
+        print(f"User said {query}\n")
+    except Exception as e:
+        print(e) 
+        print("Error")
+        return "None"
+    return query
 
 
-        try:
-            said = vocbrain.recognize_google(audio)
-            print(said)        
-        except Exception as e:
-            
-
-
-speak("a quick brown fox jumped over a lazy dog")
+speak("")
+get_audio()
 
 
